42d05ccee2f8e780959824bc27f0c7d2fa1a2bf3
Rajappagarikalpana <68804690+Rajappagarikalpana@users.noreply.github.com>
2020-07-26 13:36:37 +0530

    Add files via upload

---
+++ com.Design_Patterns_Task/src/main/java/strctural_patterns/decorator_pattern/Color.java
@@ -0,0 +1,13 @@
+package strctural_patterns.decorator_pattern;
+
+public enum Color {
+	 RED,
+	    GREEN,
+	    BLUE,
+	    YELLOW,
+	    WHITE,
+	    BLACK,
+	    ORANGE,
+	    MAROON
+
+}
---
+++ com.Design_Patterns_Task/src/main/java/strctural_patterns/decorator_pattern/Shape.java
@@ -0,0 +1,7 @@
+package strctural_patterns.decorator_pattern;
+public interface Shape {
+	void draw();
+    void resize();
+    String description();
+    boolean isHide();
+}
